bcb090db26e1a696d16bf612439e8de0c7bfa39a
Asp1kkk <a5p1k@yandex.ru>
2024-06-18 21:03:26 +0300

    add: AddExpression handler

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	entities "github.com/Aspikk/Distributed-Calculator/internal/entities/expression"
+	"github.com/Aspikk/Distributed-Calculator/internal/storage"
+)
+
+var (
+	id = 1
+)
+
+func AddExpression(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "something went wrong... (resolving body)", http.StatusInternalServerError)
+		return
+	}
+
+	exp := &entities.Expression{}
+	err = json.Unmarshal(body, exp)
+	if err != nil {
+		http.Error(w, "something went wrong... (resolving body)", http.StatusInternalServerError)
+		return
+	}
+
+	if exp.RemoveSpaces().IsInvalid() {
+		http.Error(w, "invalid expression", http.StatusUnprocessableEntity)
+		return
+	}
+	exp.SetID(&id)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	response := fmt.Sprintf("{\"id\": %d}", id-1)
+	w.Write([]byte(response))
+
+	exp.Status = "processing..."
+
+	storage.DB.Expressions = append(storage.DB.Expressions, exp)
+}
