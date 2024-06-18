08243048f58c83c0596db4a7ff770b83e91d2b8c
Asp1kkk <a5p1k@yandex.ru>
2024-06-18 21:25:29 +0300

    add: GetExpressioinById handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 
 	entities "github.com/Aspikk/Distributed-Calculator/internal/entities/expression"
 	"github.com/Aspikk/Distributed-Calculator/internal/storage"
@@ -56,4 +58,43 @@ func GetExpressions(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	w.Write(data)
+}
+
+func GetExpressioinById(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	parts := strings.Split(path, "/")
+
+	if len(parts) != 5 {
+		http.Error(w, "invalid request path", http.StatusNotFound)
+		return
+	}
+
+	idStr := parts[4]
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusNotFound)
+		return
+	}
+
+	for _, exp := range storage.DB.Expressions {
+		if exp.ID == id {
+			buf, err := json.Marshal(exp)
+			if err != nil {
+				http.Error(w, "something went wrong... (stringifying expression)", http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+
+			w.Write([]byte("{\"expression\": "))
+			w.Write(buf)
+			w.Write([]byte("}"))
+
+			return
+		}
+	}
+
+	http.Error(w, "expression not found", http.StatusNotFound)
 }
