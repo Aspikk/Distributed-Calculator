dd5abccc2b884710f3eb70227a812f3adf326b47
Asp1kkk <a5p1k@yandex.ru>
2024-07-04 19:48:17 +0300

    add: MarshallJSON method & Tasks field

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,15 +1,32 @@
 package storage
 
-import entities "github.com/Aspikk/Distributed-Calculator/internal/entities/expression"
+import (
+	"encoding/json"
+
+	expression "github.com/Aspikk/Distributed-Calculator/internal/entities/expression"
+	queue "github.com/Aspikk/Distributed-Calculator/internal/entities/queue"
+	task "github.com/Aspikk/Distributed-Calculator/internal/entities/task"
+)
 
 type DataBase struct {
-	Expressions []*entities.Expression `json:"expressions"`
+	Expressions *[]*expression.Expression `json:"expressions"`
+	Tasks       *queue.Queue[*task.Task]
+}
+
+func (db *DataBase) MarshallJSON() ([]byte, error) {
+	var tmp struct {
+		Expressions *[]*expression.Expression `json:"expressions"`
+	}
+
+	tmp.Expressions = db.Expressions
+	return json.Marshal(&tmp)
 }
 
 var DB *DataBase
 
 func init() {
 	DB = &DataBase{
-		Expressions: make([]*entities.Expression, 0),
+		Expressions: &[]*expression.Expression{},
+		Tasks:       queue.New[*task.Task](),
 	}
 }
