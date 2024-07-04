855cd34e6229ffab0584df9705b9623305cb62c8
Asp1kkk <a5p1k@yandex.ru>
2024-07-04 16:29:02 +0300

    add: Help Functions & ToRpn.

--- internal/entities/expression/expression.go
+++ internal/entities/expression/expression.go
@@ -1,9 +1,10 @@
-package entities
+package expression
 
 import (
+	"strings"
 	"unicode"
 
-	entities "github.com/Aspikk/Distributed-Calculator/internal/entities/stack"
+	stack "github.com/Aspikk/Distributed-Calculator/internal/entities/stack"
 )
 
 type Expression struct {
@@ -11,18 +12,38 @@ type Expression struct {
 	Result int    `json:"result"`
 	Status string `json:"status"`
 	Raw    string `json:"expression"`
+	rpn    string // Reverse Polish Notation
 }
 
-func (e *Expression) RemoveSpaces() *Expression {
+// Methods
+
+func (e *Expression) AddSpaces() {
 	res := ""
 	for _, v := range e.Raw {
 		if v != ' ' {
 			res += string(v)
 		}
 	}
-	e.Raw = res
 
-	return e
+	e.Raw = ""
+
+	for i, v := range res {
+		if unicode.IsDigit(v) {
+			e.Raw += string(v)
+		} else {
+			idx := i + 1
+			if i == len(res)-1 {
+				idx = i
+			}
+			if unicode.IsDigit(rune(res[idx])) {
+				e.Raw += " " + string(v) + " "
+			} else {
+				e.Raw += " " + string(v)
+			}
+		}
+	}
+
+	e.Raw = strings.Trim(e.Raw, " ")
 }
 
 func (e *Expression) SetID(id *int) {
@@ -46,9 +67,15 @@ func (e *Expression) IsInvalid() bool {
 		return true
 	}
 
-	stack := entities.NewStack[rune]()
+	stack := stack.NewStack[rune]()
+
+	operationCount := 0
 
 	for i, v := range e.Raw {
+		if v == ' ' {
+			continue
+		}
+
 		if !(unicode.IsDigit(rune(v)) || v == '+' || v == '-' || v == '*' || v == '/' || v == '(' || v == ')' || v == '^') {
 			return true
 		}
@@ -61,6 +88,8 @@ func (e *Expression) IsInvalid() bool {
 			if (i > 0) && (e.Raw[i-1] == '(' || e.Raw[i-1] == '+' || e.Raw[i-1] == '-' || e.Raw[i-1] == '*' || e.Raw[i-1] == '/' || e.Raw[i-1] == '^') {
 				return true
 			}
+
+			operationCount++
 		}
 
 		if v == '(' {
@@ -80,8 +109,72 @@ func (e *Expression) IsInvalid() bool {
 		return true
 	}
 
+	if operationCount < 1 {
+		return true
+	}
+
 	return !stack.IsEmpty()
 }
+
+func (e *Expression) ToRpn() {
+	var priorities map[string]int = map[string]int{
+		"^": 4,
+		"*": 3,
+		"/": 3,
+		"+": 2,
+		"-": 2,
+		"(": 1,
+	}
+
+	stack := stack.NewStack[string]()
+
+	for _, v := range strings.Split(e.Raw, " ") {
+		if isStringDigit(v) {
+			e.rpn += v + " "
+		} else {
+			if v == "(" {
+				stack.Push(v)
+				continue
+			} else if v == ")" {
+				for {
+					if stack.GetTop() == "(" {
+						stack.Pop()
+						break
+					}
+					val, _ := stack.Pop()
+					e.rpn += val + " "
+				}
+				continue
+			}
+
+			if stack.IsEmpty() || priorities[stack.GetTop()] < priorities[v] {
+				stack.Push(v)
+			} else {
+				for val, is := stack.Pop(); priorities[val] >= priorities[v] && is; {
+					if val == "(" {
+						val, is = stack.Pop()
+						continue
+					}
+					e.rpn += val + " "
+					val, is = stack.Pop()
+				}
+				stack.Push(v)
+			}
+		}
+	}
+
+	for val, is := stack.Pop(); is; {
+		if val == ")" {
+			val, is = stack.Pop()
+			continue
+		}
+
+		e.rpn += val + " "
+		val, is = stack.Pop()
+	}
+}
+
+// Help functions
 
 func OnlyParentheses(expression string) bool {
 	hasOnlyParenthesis := true
@@ -94,4 +187,13 @@ func OnlyParentheses(expression string) bool {
 	}
 
 	return hasOnlyParenthesis
+}
+
+func isStringDigit(s string) bool {
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+	}
+	return true
 }
