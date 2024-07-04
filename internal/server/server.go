c3d1de9b69a0da325bbe91e565fc5ee25105ba9e
Asp1kkk <a5p1k@yandex.ru>
2024-07-04 17:41:50 +0300

    add: refactored

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/Aspikk/Distributed-Calculator/internal/handlers"
+	handlers "github.com/Aspikk/Distributed-Calculator/internal/handlers"
 )
 
 type Server struct {
