47ba8413913f50b398001d67bfaea513361d7072
Asp1kkk <a5p1k@yandex.ru>
2024-07-04 16:31:26 +0300

    add: refactored

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -8,13 +8,15 @@ import (
 	"strconv"
 	"strings"
 
-	entities "github.com/Aspikk/Distributed-Calculator/internal/entities/expression"
-	"github.com/Aspikk/Distributed-Calculator/internal/storage"
+	expression "github.com/Aspikk/Distributed-Calculator/internal/entities/expression"
+	storage "github.com/Aspikk/Distributed-Calculator/internal/storage"
 )
 
 var (
 	id = 1
 )
+
+// Handlers
 
 func AddExpression(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
@@ -23,53 +25,43 @@ func AddExpression(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	exp := &entities.Expression{}
+	exp := &expression.Expression{}
 	err = json.Unmarshal(body, exp)
 	if err != nil {
 		http.Error(w, "something went wrong... (resolving body)", http.StatusInternalServerError)
 		return
 	}
 
-	if exp.RemoveSpaces().IsInvalid() {
+	if exp.AddSpaces(); exp.IsInvalid() {
 		http.Error(w, "invalid expression", http.StatusUnprocessableEntity)
 		return
 	}
-	exp.SetID(&id)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+	exp.SetID(&id)
+	exp.SetStatus("processing")
+	exp.AddTo(storage.DB.Expressions)
+	exp.ToRpn()
 
 	response := fmt.Sprintf("{\"id\": %d}", id-1)
-	w.Write([]byte(response))
-
-	exp.Status = "processing..."
-
-	storage.DB.Expressions = append(storage.DB.Expressions, exp)
+	writeResponse(w, http.StatusCreated, []byte(response))
 }
 
 func GetExpressions(w http.ResponseWriter, r *http.Request) {
-	data, err := json.Marshal(storage.DB)
+	data, err := storage.DB.MarshallJSON()
 	if err != nil {
 		http.Error(w, "something went wrong... (stringifying storage)", http.StatusInternalServerError)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	w.Write(data)
+	writeResponse(w, http.StatusOK, data)
 }
 
 func GetExpressioinById(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	parts := strings.Split(path, "/")
-
-	if len(parts) != 5 {
+	idStr, ok := getId(r)
+	if !ok {
 		http.Error(w, "invalid request path", http.StatusNotFound)
 		return
 	}
-
-	idStr := parts[4]
 
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
@@ -77,7 +69,7 @@ func GetExpressioinById(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, exp := range storage.DB.Expressions {
+	for _, exp := range *storage.DB.Expressions {
 		if exp.ID == id {
 			buf, err := json.Marshal(exp)
 			if err != nil {
@@ -85,16 +77,31 @@ func GetExpressioinById(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-
-			w.Write([]byte("{\"expression\": "))
-			w.Write(buf)
-			w.Write([]byte("}"))
-
+			result := fmt.Sprintf("{\"expression\": %s}", string(buf))
+			writeResponse(w, http.StatusOK, []byte(result))
 			return
 		}
 	}
 
 	http.Error(w, "expression not found", http.StatusNotFound)
+}
+
+// Help functions
+
+func writeResponse(w http.ResponseWriter, status int, data []byte) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	w.Write(data)
+}
+
+func getId(r *http.Request) (string, bool) {
+	path := r.URL.Path
+	parts := strings.Split(path, "/")
+
+	if len(parts) != 5 {
+		return "", false
+	}
+
+	return parts[4], true
 }
